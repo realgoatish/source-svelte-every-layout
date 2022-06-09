c6f7e94c45c1344cfcdd040e41972e47035077d2
Michael Lamb <mflambjr@gmail.com>
2022-06-09 00:40:11 -0400

    testing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,52 @@
 <script lang='ts'>
-  import Center from '$prod/components/Center.svelte'
-  import Sidebar from '$prod/components/Sidebar.svelte'
-  import Box from '$prod/components/Box.svelte'
+  // import Center from '$prod/components/Center.svelte'
+  // import Cluster from '$prod/components/Cluster.svelte'
+  // import Box from '$prod/components/Box.svelte'
+  import Center from '$lib/components/Center.svelte'
+  import Grid from '$lib/components/Grid.svelte'
+  import Box from '$lib/components/Box.svelte'
 </script>
 
-<Center wrapperClass={"yes"} andText={true} >
-  <Box colorLight={1} invert>
-    <Sidebar sideWidth={undefined}>
-      <h1>Welcome to SvelteKit</h1>
-      <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    </Sidebar>
+<!-- <Center max="20rem" intrinsic> -->
+  <div>
+    <Grid wrapperElement="ul" min="650px">
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+      <Box colorLight={"pink"}>
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+      </Box>
+    </Grid>
 
-  </Box>
+  </div>
 
-</Center>
+
+<!-- </Center> -->
 
 <style>
-  :global(.center) {
-    --measure: 25ch;
+  div {
+    width: 100%;
   }
 </style>
